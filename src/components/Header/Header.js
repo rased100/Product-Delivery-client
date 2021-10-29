2cf31e012660ccc4b0e93fe6c9045f60c6ba9688
rased100 <rased100@gmail.com>
2021-10-29 19:40:58 +0600

    logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import useAuth from '../Hooks/useAuth';
 import './Header.css';
 
 const Header = () => {
+    const { user, logOut } = useAuth();
+    console.log(user)
     return (
         <div className="p-3 d-flex justify-content-evenly align-items-center">
             <div>
@@ -15,7 +18,19 @@ const Header = () => {
                 <NavLink className="header-link" to="/contact">Contact</NavLink>
             </div>
             <div>
-                <NavLink className="header-link" to="/login">Login</NavLink>
+                {user?.displayName ?
+                    <span>
+                        <span className="mx-3">
+                            User Name: {user.displayName}
+                        </span>
+                        <span>
+                            <button onClick={logOut} className="btn btn-info">Logout</button>
+                        </span>
+                    </span>
+
+                    :
+                    <NavLink className="header-link" to="/login">Login</NavLink>
+                }
             </div>
         </div>
     );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useAuth from '../Hooks/useAuth';
+import { useHistory, useLocation } from 'react-router';
 
 const Login = () => {
+    const { handleGoogleSignIn } = useAuth();
+    const location = useLocation();
+    const history = useHistory();
+    const redirectUrl = location.state?.from || '/home';
+
+    const handleGoogleSignInTwo = () => {
+        handleGoogleSignIn()
+            .then((result) => {
+                history.push(redirectUrl);
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
     return (
         <div>
             <div className="container w-25">
@@ -9,9 +25,7 @@ const Login = () => {
                 <input className="form-control m-2" type="password" placeholder="Your password" />
                 <input className="btn btn-info form-control m-2" type="submit" value="Submit" />
                 <div className="m-5">------ or ------</div>
-                <button
-                    /* onClick={handleGoogleSignInHistory} */
-                    className="btn btn-info form-control m-2 mb-5">Sign in with google</button>
+                <button onClick={handleGoogleSignInTwo} className="btn btn-info form-control m-2 mb-5">Sign in with google</button>
 
 
             </div>
