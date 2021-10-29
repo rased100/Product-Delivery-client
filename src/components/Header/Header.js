4f17aa0d72dbe68cdff0cdfdf1735758b3ac07c0
rased100 <rased100@gmail.com>
2021-10-29 10:56:11 +0600

    first commit Header-done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="p-3 d-flex justify-content-evenly align-items-center">
+            <div>
+                <h2><span className="text-danger fw-bold">CURRENT</span> <span className="title">Delivery</span></h2>
+
+            </div>
+            <div>
+                <NavLink className="header-link" to="/home">Home</NavLink>
+                <NavLink className="header-link" to="/about">About</NavLink>
+                <NavLink className="header-link" to="/Contact">Contact</NavLink>
+            </div>
+            <div>
+                <NavLink className="header-link" to="/login">Login</NavLink>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
