10978694e5f1a0134a807f57032766af082825c8
rased100 <rased100@gmail.com>
2021-10-29 11:50:29 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header></Header>
+        <Home></Home>
       </Router>
     </div>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="banner d-flex align-items-center">
+            <h1 className="w-50 p-3">Get your courier delivered with the utmost safety with us</h1>
+        </div>
+    );
+};
+
+export default Home;
